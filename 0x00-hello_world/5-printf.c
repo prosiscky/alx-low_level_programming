de0fe07743b56e7eb9355a025fe5552923d65e07
prosiscky <legendaryprosperity001@gmail.com>
2022-10-13 17:57:53 -0700

    correction printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main() This function uses printf to print text to screen
+ * main-entry point of the program
  *
- * Return:0 The value 0 is returned if the code compiles successfully
+ * Return:0 if program compiles without error
 */
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
