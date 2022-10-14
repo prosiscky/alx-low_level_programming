8e2e5cc66794ca85cb122b32285696a4613c3f7e
prosiscky <legendaryprosperity001@gmail.com>
2022-10-13 17:38:15 -0700

    using the function printf to print text to screen

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main() This function uses printf to print text to screen
+ *
+ * Return: 0 The value 0 is returned if the code compiles successfully
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0)
+}
