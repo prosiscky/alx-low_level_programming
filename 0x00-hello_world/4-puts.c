fa908a75afdbd18b357458a96ed3fda0124f9422
prosiscky <legendaryprosperity001@gmail.com>
2022-10-13 17:24:37 -0700

    Using the command 'puts' to print text to standard output

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main()- This function uses function 'puts' to write a text to screen
+ *
+ * Return: 0 returns 0 if code compiles without error
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
