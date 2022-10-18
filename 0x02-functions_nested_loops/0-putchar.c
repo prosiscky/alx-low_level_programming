319bb381980d278f20c97fc1711c7842a809c313
prosiscky <legendaryprosperity001@gmail.com>
2022-10-18 16:41:40 -0700

    update task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,28 +1,20 @@
 #include "main.h"
 /**
  * _putchar - A function to print character
- * @c: A character to be printed
+ *
  * Return: 0 on successful compilation
 */
 
-int _putchar(char c)
+int main(void)
 {
-	if (c == '_')
-		_putchar('_');
-	else if (c == 'p')
-		_putchar('p');
-	else if (c == 'u')
-		_putchar('u');
-	else if (c == 't')
-		_putchar('t');
-	else if (c == 'c')
-		_putchar('c');
-	else if (c == 'h')
-		_putchar('h');
-	else if (c == 'a')
-		_putchar('a');
-	else if (c == 'r')
-		_putchar('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
