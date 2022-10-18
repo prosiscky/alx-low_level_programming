b83d7722459fd43cc7621e3d122464a479a28ed7
prosiscky <legendaryprosperity001@gmail.com>
2022-10-18 15:43:36 -0700

    Readme, header file and 0-putchar are being commited

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _putchar - A function to print character
+ *
+ * @c: argument to be passed to the function
+ *
+ * Return: 0 on successful compilation
+*/
+
+int _putchar(char c)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
