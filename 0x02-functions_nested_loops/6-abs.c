bc2fe58781af59e21a51428c68227fbf9943cd22
prosiscky <legendaryprosperity001@gmail.com>
2022-10-18 20:20:28 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_abs-prints the absolute value of an entered number.
+ *@i:the integer to compute its absolute value.
+ *Return:returns the abs value.
+ *
+*/
+
+int _abs(int i)
+{
+	int i = 0;
+	int absval;
+
+	if (i < 0)
+	{
+		k = k * (-1);
+	}
+	absval = k;
+	return (absval);
+}
