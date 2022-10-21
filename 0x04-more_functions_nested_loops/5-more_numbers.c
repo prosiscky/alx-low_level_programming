af2adaef7fcc2590c795c15d0c47d25678cd0e99
prosiscky <legendaryprosperity001@gmail.com>
2022-10-20 20:44:18 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,16 +5,15 @@
 
 void more_numbers(void)
 {
-	int i;
-	int j;
+	int i, j;
 
 
 	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			j = j + '0'
-			_putchar(j);
+			_putchar(j + '0');
 		}
 	}
+	_putchar('\n');
 }
