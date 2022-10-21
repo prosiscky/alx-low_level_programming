0a674ebf53e9f941100738a532e6b10fc8e40e97
prosiscky <legendaryprosperity001@gmail.com>
2022-10-20 20:06:06 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints num 0-9 skip 2and 4
+ */
+
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (c == 2 || c == 4)
+		{
+			continue;
+			_putchar(c);
+		}
+	}
+	_putchar('\n');
+}
