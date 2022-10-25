6ef2cdccd1786a0a4054e7621a0984e5d756cbe7
prosiscky <legendaryprosperity001@gmail.com>
2022-10-24 18:23:34 -0700

    readme and task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *reset_to_98 - updates the value it points to to 98
+ *@n: variable whose values is to be updated
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This folder holds files for practicing pointers, arrays and strings
