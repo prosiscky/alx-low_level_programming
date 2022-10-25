640554dcc005695b9b1f59e71bff92e0134d07d5
prosiscky <legendaryprosperity001@gmail.com>
2022-10-24 20:11:22 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: string to be printed
+ * return: void
+*/
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = \0; i <= 0; i++)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+	return (void);
+}
