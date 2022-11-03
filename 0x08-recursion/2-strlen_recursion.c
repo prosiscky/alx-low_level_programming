110a40df895ae5feb717fc4322109a4e87143e6c
prosiscky <legendaryprosperity001@gmail.com>
2022-11-02 19:50:31 -0700

    Task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -11,9 +11,8 @@ int _strlen_recursion(char *s)
 
 	if (*s != '\0')
 	{
-		count = count + 1;
-		s++;
-		_strlen_recursion(s);
+		count++
+		count += _strlen_recursion(s + 1);
 	}
 	return (count);
 }
