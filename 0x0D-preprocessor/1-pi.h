73d2cf09902d3e40980c3483bd81671c20f76690
prosiscky <legendaryprosperity001@gmail.com>
2022-11-14 20:40:45 -0800

    Task 1

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,5 +1,11 @@
-#ifndef PI
-#define PI
+#ifndef PI_H
+#define PI_H
+
+/*
+ * File: 1-pi.h
+ * Desc: Header file that defines a macro named PI
+ *       as an abbreviation for the token 3.14159265359.
+ */
 
 #define PI 3.14159265359
 
