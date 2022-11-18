dac495c0189f6ceb394fd766b2a1c7201dccef52
prosiscky <legendaryprosperity001@gmail.com>
2022-11-17 19:58:51 -0800

    Task 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,7 @@
-#ifndef variadic_functions_h
-#define variadic_functions_h
+#ifndef _VARIADIC_FUNCTIONS_
+#define _VARIADIC_FUNCTIONS_
+#include <stdarg.h>
+#include <stdio.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
